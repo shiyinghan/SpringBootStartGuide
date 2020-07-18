825ddcaea859ba1b06a6c356b200a4f06001e28c
hanshiying <8811782@163.com>
2020-07-18 17:56:41 +0800

    uploading-files

---
+++ uploading-files/src/main/java/com/hansy/uploadingfiles/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.hansy.uploadingfiles.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
