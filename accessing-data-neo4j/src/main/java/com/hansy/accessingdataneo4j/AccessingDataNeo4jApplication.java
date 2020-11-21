d2b8be4c7f78d34b926e6e4b80c54dde9a503a26
hanshiying <8811782@163.com>
2020-11-21 15:59:48 +0800

    Spring Boot入门教程：第九章：访问Neo4j图形数据库

---
+++ accessing-data-neo4j/src/main/java/com/hansy/accessingdataneo4j/AccessingDataNeo4jApplication.java
@@ -0,0 +1,64 @@
+package com.hansy.accessingdataneo4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@EnableNeo4jRepositories
+public class AccessingDataNeo4jApplication {
+
+    private final static Logger log = LoggerFactory.getLogger(AccessingDataNeo4jApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataNeo4jApplication.class, args);
+        System.exit(0);
+    }
+
+    @Bean
+    CommandLineRunner demo(PersonRepository personRepository) {
+        return args -> {
+            personRepository.deleteAll();
+
+            Person greg = new Person("Greg");
+            Person roy = new Person("Roy");
+            Person craig = new Person("Craig");
+
+            List<Person> team = Arrays.asList(greg, roy, craig);
+
+            log.info("Before linking up with Neo4j...");
+
+            team.forEach(person -> log.info("\t" + person.toString()));
+
+            personRepository.save(greg);
+            personRepository.save(roy);
+            personRepository.save(craig);
+
+            greg = personRepository.findByName(greg.getName());
+            greg.workWith(roy);
+            greg.workWith(craig);
+            personRepository.save(greg);
+
+            roy = personRepository.findByName(roy.getName());
+            roy.workWith(craig);
+            //We already know that roy works with greg
+            personRepository.save(roy);
+
+            //We already know craig works with roy and greg
+
+            log.info("Lookup each person by name...");
+            team.forEach(person -> log.info("\t" + personRepository.findByName(person.getName()).toString()));
+
+            List<Person> teammates = personRepository.findByTeammatesName(greg.getName());
+            log.info("The following have Greg as a teammate...");
+            teammates.forEach(person -> log.info("\t" + person.getName()));
+        };
+    }
+}
